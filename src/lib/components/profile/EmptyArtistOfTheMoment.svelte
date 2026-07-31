d3b0974be070adcc2e4b74230652860895ea0a3f
Igor Araújo <igorgsaaraujo001@gmail.com>
2026-07-31 12:05:37 -0300

    feat: Adjusting artist section responsiveness

--- src/lib/components/profile/EmptyArtistOfTheMoment.svelte
+++ src/lib/components/profile/EmptyArtistOfTheMoment.svelte
@@ -1,38 +1,38 @@
 <script lang="ts">
-	// Assets
-	import SelectArtistIcon from '$lib/assets/images/icons/SelectArtistIcon.svelte';
+    // Assets
+    import SelectArtistIcon from '$lib/assets/images/icons/SelectArtistIcon.svelte';
 
-	// Stores
-	import { translationsStore } from '$lib/stores/translations.store';
+    // Stores
+    import { translationsStore } from '$lib/stores/translations.store';
 
-	// Props
-	export let openSelectYourItemsModal: (itemType: 'artist') => void;
+    // Props
+    export let openSelectYourItemsModal: (itemType: 'artist') => void;
 </script>
 
-<div class="flex w-full flex-col gap-4 2xl:w-2/6">
-	<h3 class="text-xs font-semibold tracking-[0.18em] text-t-secondary uppercase">
-		{$translationsStore.profilePage.profilePageEmptyArtistOfTheMomentHeading1}
-	</h3>
+<div class="flex w-full flex-col gap-3 sm:gap-4 xl:w-1/3">
+    <h3 class="text-xs font-semibold tracking-[0.18em] text-t-secondary uppercase">
+        {$translationsStore.profilePage.profilePageEmptyArtistOfTheMomentHeading1}
+    </h3>
 
-	<button
-		class="flex h-full cursor-pointer flex-col items-center justify-center gap-2 rounded-xl border border-b-default p-6 text-t-primary shadow-xl md:max-h-56 md:min-h-56"
-		on:click={() => openSelectYourItemsModal('artist')}
-	>
-		<div class="flex h-20 w-20 items-center justify-center rounded-full bg-s-muted">
-			<SelectArtistIcon
-				iconSvgClass="ml-1.5 h-10 w-10"
-				iconAltText={$translationsStore.profilePage.profilePageEmptyArtistOfTheMomentAltText}
-			/>
-		</div>
+    <button
+        class="flex h-full cursor-pointer flex-col items-center justify-center gap-2 rounded-xl border border-b-default p-5 text-t-primary shadow-xl sm:p-6 md:min-h-56 md:max-h-56"
+        on:click={() => openSelectYourItemsModal('artist')}
+    >
+        <div class="flex h-16 w-16 items-center justify-center rounded-full bg-s-muted sm:h-20 sm:w-20">
+            <SelectArtistIcon
+                iconSvgClass="ml-1.5 h-8 w-8 sm:h-10 sm:w-10"
+                iconAltText={$translationsStore.profilePage.profilePageEmptyArtistOfTheMomentAltText}
+            />
+        </div>
 
-		<div class="max-w-xs text-center">
-			<h4 class="font-semibold">
-				{$translationsStore.profilePage.profilePageEmptyArtistOfTheMomentHeading2}
-			</h4>
+        <div class="max-w-[16rem] text-center sm:max-w-xs">
+            <h4 class="font-semibold">
+                {$translationsStore.profilePage.profilePageEmptyArtistOfTheMomentHeading2}
+            </h4>
 
-			<p class="mt-1 text-xs leading-relaxed text-t-secondary">
-				{$translationsStore.profilePage.profilePageEmptyArtistOfTheMomentParagraph1}
-			</p>
-		</div>
-	</button>
+            <p class="mt-1 text-xs leading-relaxed text-t-secondary">
+                {$translationsStore.profilePage.profilePageEmptyArtistOfTheMomentParagraph1}
+            </p>
+        </div>
+    </button>
 </div>
--- src/lib/components/profile/YourArtistsOnProfile.svelte
+++ src/lib/components/profile/YourArtistsOnProfile.svelte
@@ -1,48 +1,48 @@
 <script lang="ts">
-	// Components
-	import YourArtistOnProfileItem from './YourArtistsOnProfileItem.svelte';
-	import EmptyArtistOfTheMoment from './EmptyArtistOfTheMoment.svelte';
-	import EmptyCustomArtist from './EmptyCustomArtist.svelte';
+    // Components
+    import YourArtistOnProfileItem from './YourArtistsOnProfileItem.svelte';
+    import EmptyArtistOfTheMoment from './EmptyArtistOfTheMoment.svelte';
+    import EmptyCustomArtist from './EmptyCustomArtist.svelte';
 
-	// Stores
-	import { userInfo } from '$lib/stores/userInfo.store';
-	import { translationsStore } from '$lib/stores/translations.store';
+    // Stores
+    import { userInfo } from '$lib/stores/userInfo.store';
+    import { translationsStore } from '$lib/stores/translations.store';
 
-	// Props
-	export let openChangeYourItemsModal: (itemType: 'artist') => void;
-	export let openSelectYourItemsModal: (itemType: 'artist') => void;
-	export let openChangeCustomItemModal: (itemType: 'artist') => void;
-	export let openSelectCustomItemModal: (itemType: 'artist') => void;
+    // Props
+    export let openChangeYourItemsModal: (itemType: 'artist') => void;
+    export let openSelectYourItemsModal: (itemType: 'artist') => void;
+    export let openChangeCustomItemModal: (itemType: 'artist') => void;
+    export let openSelectCustomItemModal: (itemType: 'artist') => void;
 
-	$: artistItems = [
-		{
-			artistItem:
-				$userInfo?.connectedStreamings.spotify?.mostListenedArtists?.mostListenedArtistItem,
-			type: 'mostListenedArtist'
-		},
-		{ artistItem: $userInfo?.artistOfTheMoment, type: 'artistOfTheMoment' },
-		{ artistItem: $userInfo?.customArtist?.artist, type: 'customArtist' }
-	];
+    $: artistItems = [
+        {
+            artistItem:
+                $userInfo?.connectedStreamings.spotify?.mostListenedArtists?.mostListenedArtistItem,
+            type: 'mostListenedArtist'
+        },
+        { artistItem: $userInfo?.artistOfTheMoment, type: 'artistOfTheMoment' },
+        { artistItem: $userInfo?.customArtist?.artist, type: 'customArtist' }
+    ];
 </script>
 
-<section class="space-y-6">
-	<h2 class="text-2xl font-semibold text-t-primary">
-		{$translationsStore.profilePage.profilePageYourArtistsOnProfileHeading2}
-	</h2>
+<section class="space-y-4 sm:space-y-6">
+    <h2 class="text-xl font-semibold text-t-primary sm:text-2xl">
+        {$translationsStore.profilePage.profilePageYourArtistsOnProfileHeading2}
+    </h2>
 
-	<div class="flex flex-col gap-8 xl:flex-row">
-		{#each artistItems as { artistItem, type }}
-			{#if artistItem}
-				<YourArtistOnProfileItem
-					artistItem={{ artist: artistItem, type }}
-					{openChangeYourItemsModal}
-					{openChangeCustomItemModal}
-				/>
-			{:else if type === 'artistOfTheMoment'}
-				<EmptyArtistOfTheMoment {openSelectYourItemsModal} />
-			{:else if type === 'customArtist'}
-				<EmptyCustomArtist {openSelectCustomItemModal} />
-			{/if}
-		{/each}
-	</div>
+    <div class="flex flex-col gap-4 sm:gap-6 xl:flex-row xl:gap-8">
+        {#each artistItems as { artistItem, type }}
+            {#if artistItem}
+                <YourArtistOnProfileItem
+                    artistItem={{ artist: artistItem, type }}
+                    {openChangeYourItemsModal}
+                    {openChangeCustomItemModal}
+                />
+            {:else if type === 'artistOfTheMoment'}
+                <EmptyArtistOfTheMoment {openSelectYourItemsModal} />
+            {:else if type === 'customArtist'}
+                <EmptyCustomArtist {openSelectCustomItemModal} />
+            {/if}
+        {/each}
+    </div>
 </section>
