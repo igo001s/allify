0fcc125e607afcd60275a39eba8840edddefe8c8
Igor Araújo <igorgsaaraujo001@gmail.com>
2026-08-17 20:41:37 -0300

    chore: format

--- src/routes/sitemap.xml/+server.ts
+++ src/routes/sitemap.xml/+server.ts
@@ -6,15 +6,15 @@ import { ALLIFY_URL } from '$env/static/private';
 
 export const GET: RequestHandler = async () => {
 	const pages = [
-		{name: '', lastMod: '2026-08-16'},
-		{name: '/my-musical-profile', lastMod: '2026-08-16'},
-		{name: '/discoveries', lastMod: '2026-08-16'},
-		{name: '/music-community', lastMod: '2026-08-16'},
-		{name: '/profile', lastMod: '2026-08-16'},
-		{name: '/settings', lastMod: '2026-08-16'},
-		{name: '/privacy-policy', lastMod: '2026-08-16'},
-		{name: '/terms-of-service', lastMod: '2026-08-16'},
-		{name: '/data-usage', lastMod: '2026-08-16'}
+		{ name: '', lastMod: '2026-08-16' },
+		{ name: '/my-musical-profile', lastMod: '2026-08-16' },
+		{ name: '/discoveries', lastMod: '2026-08-16' },
+		{ name: '/music-community', lastMod: '2026-08-16' },
+		{ name: '/profile', lastMod: '2026-08-16' },
+		{ name: '/settings', lastMod: '2026-08-16' },
+		{ name: '/privacy-policy', lastMod: '2026-08-16' },
+		{ name: '/terms-of-service', lastMod: '2026-08-16' },
+		{ name: '/data-usage', lastMod: '2026-08-16' }
 	];
 
 	const body = `<?xml version="1.0" encoding="UTF-8" ?>
